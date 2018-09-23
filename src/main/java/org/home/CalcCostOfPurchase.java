a6df64b11b7027f4a8ddb2956f4c06ec0ac64900
Viktoriya <viktoriyaterescenko@gmail.com>
2018-09-23 21:59:34 +0300

    first commit

---
+++ src/main/java/org/home/CalcCostOfPurchase.java
@@ -0,0 +1,26 @@
+package org.home;
+
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
+import java.util.Scanner;
+
+public class CalcCostOfPurchase {
+
+    public static void calcCostOfPurchase(double costOfCandies, double costOfCookies) {
+        double onePurchase = costOfCandies * 0.3 + costOfCookies * 0.4;
+        double threePurchases = (costOfCandies * 1.8 + costOfCookies * 2) * 3;
+        System.out.println("I am friendly, so cost of 1 purchase = " + onePurchase + " grn");
+        System.out.println("I am friendly, so cost of 3 purchases = " + threePurchases + " grn");
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Enter cost of candies(kg): ");
+        double costOfCandies = console.nextDouble();
+        System.out.println("Enter cost of cooclies (kg): ");
+        double costOfCookies = console.nextDouble();
+        calcCostOfPurchase(costOfCandies, costOfCookies);
+    }
+}
---
+++ src/main/java/org/home/Velocity.java
@@ -0,0 +1,28 @@
+package org.home;
+
+import java.util.Scanner;
+
+public class Velocity {
+
+    public static void calcVelocity(double time, double range) {
+        double rangeKmTom = range / 1000;
+        double timeMToS = time * 60;
+        double vel = rangeKmTom / timeMToS;
+        System.out.println("Velocity  = " + vel + " m/s");
+    }
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Enter time(m): ");
+
+
+        double time = console.nextDouble();
+        System.out.println("Enter range (km): ");
+        double range = console.nextDouble();
+        calcVelocity(time, range);
+    }
+}
+
+
+
+
