d9328a2b3f526364adf01eaa48a536c102dbaab6
Viktoriya <viktoriyaterescenko@gmail.com>
2018-09-24 00:12:52 +0300

    home1

--- src/main/java/org/home1/Decompose.java
+++ src/main/java/org/home1/Decompose.java
@@ -1,4 +1,38 @@
 package org.home1;
 
+
+//Составить программу, которая будет разлаживать 5-ти значное число на отдельные цифры данного числа, справа-налево. Число задается в параметре статического метода.
+//Каждую цифру вывести в отдельной строке.
+//45685
+//5
+//8
+//6
+//5
+//4
+
+
 public class Decompose {
+
+
+    public static void dec( int number) {
+        String text = Integer.toString(number);
+        int i = 4;
+        int length = Integer.toString(number).length();
+        while (i < length) {
+            char num = text.charAt(i--);
+            System.out.println(num);
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        dec(12929);
+    }
+
 }
+
+
+
+
+
--- src/main/java/org/home1/Equation.java
+++ src/main/java/org/home1/Equation.java
@@ -1,4 +1,73 @@
 package org.home1;
+import java.util.Scanner;
 
-public class equation {
+//Задание 1.
+//Написать программу решающую следующие уравнения. Переменные x,y,z,n инициализируется перед решением уравнения.
+// Каждое решение оформить в отдельном статическом методе.
+//x + (y - 100) * z / n  		=> считаем в long
+//(x - (50 + y * z) * n) + x 	=> считаем в int
+//x * (y / (z-1) + 555) - n 	=> считаем в float
+//-x / y + z * (n+1) 		=> считаем в double
+
+
+public class Equation {
+
+    public static void eqtn1(int x, int y, int z,int n) {
+        long eq1 = (long) ((x + (y - 100) * z / n));
+        System.out.println(eq1);
+//        System.out.printf("The solution of the 1 Equation: %n %eq1", eq1);
+    }
+
+
+    public static void eqtn2(int x, int y, int z,int n) {
+        int eq2 = (x - (50 + y * z) * n) + x;
+        System.out.println(eq2);
+//        System.out.printf("The solution of the 2 Equation: %n", eq2);
+
+    }
+
+    public static void eqtn3(int x, int y, int z,int n) {
+        float eq3 = (float)(x * (y / (z-1) + 555) - n);
+        System.out.println(eq3);
+
+//        System.out.printf("The solution of the 3 Equation: %n", eq3);
+    }
+
+
+    public static void eqtn4(int x, int y, int z,int n) {
+        double eq4 = (double)(-x / y + z * (n+1));
+        System.out.println(eq4);
+
+//        System.out.printf("The solution of the 4 Equation: %n", eq4);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Please, enter x,y,z and n:");
+        int x = console.nextInt();
+        int y = console.nextInt();
+        int z = console.nextInt();
+        int n = console.nextInt();
+
+        eqtn1(x,y,z,n);
+        eqtn2(x,y,z,n);
+        eqtn3(x,y,z,n);
+        eqtn4(x,y,z,n);
+
+
+    }
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
