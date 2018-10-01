9bcf3ac5706ee6946555d05d037937014ed41e4e
Viktoriya <viktoriyaterescenko@gmail.com>
2018-10-01 15:00:10 +0300

    MinMax

--- src/main/java/org/home/assignment2/MinMax.java
+++ src/main/java/org/home/assignment2/MinMax.java
@@ -1,62 +1,45 @@
 package org.home.assignment2;
 
 
-
 //Есть целое положительное число (long). Программа должна определить наименьшую и наибольшую цифры, которые входят в состав данного числа.
 //
 //        У числа 15875:
 //        Наименьшее число: 1
 //        Наибольшее число: 8
 
+import java.util.Scanner;
+
 public class MinMax {
+    public static long data;
+    public static long min = 9;
+    public static long max = 0;
 
 
-
-    public static void task3() {
-
-
-
-
-
-
-
-        
-
-
-        int[] array = new int[]{200, 10, 11, 99, 11};
-        int min = array[0];
-
-        for(int i = 0; i < array.length; ++i) {
-            if (min > array[i]) {
-                min = array[i];
-            }
-        }
-        System.out.println(min);
-
-
-
-
-
-
+    public static void getNumber() {
+        System.out.println("Please, enter number:");
+        data = new Scanner(System.in).nextLong();
     }
 
-
-
+    public static void divide() {
+        while (data > 0) {
+            long current = data % 10;
+            if (current > max) {
+                max = current;
+            }
+            if (current < min){
+                min = current;
+            }
+            data = data / 10;
+        }
+    }
 
     public static void main(String[] args) {
-
-
-        task3();
-
-
-
-
-
-
-
-
-
+        getNumber();
+        divide();
+        System.out.printf("The min value is : %s%nThe max value is : %s%n", min,max);
 
 
     }
+
 }
+
